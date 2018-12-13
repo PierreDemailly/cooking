60cc6c743e8771de9083c8fb6a2fd5f6c57865c5
Paul <decouxpaul0@gmail.Com>
2018-12-13 11:44:13 +0100

    css

--- views/recipeVue.php
+++ views/recipeVue.php
@@ -1,18 +1,18 @@
 <section>
-
-    <div class="flex-wrap mt-5 p-3 d-flex justify-content-center">
-        
-        <?php if(isset($recipes)) foreach($recipes as $recipe): ?>
-            <div class="card m-2" style="width: 18rem;">
-                <img class="card-img-top" src="<?= $path ?>/assets/img/<?php echo $recipe->getPicture(); ?>" alt="Card image cap">
-                <a class="card-body" href="<?= $path ?>/recipe_description/<?php echo $recipe->getId(); ?>">
-                    <div class="">
-                        <p class="text-center text-white font-weight-bold card-text"><?php echo $recipe->getname(); ?></p>
-                    </div>
-                </a>
-            </div>
-        <?php endforeach; ?>
-
-    </div>
+    <div class="container">
+        <div class="flex-wrap p-3 d-flex justify-content-center">
             
+            <?php if(isset($recipes)) foreach($recipes as $recipe): ?>
+                    <a class="card border border-dark m-2" href="<?= $path ?>/recipe_description/<?php echo $recipe->getId(); ?>">
+                        <div class="size_div">
+                                <div class="card-body">
+                                <p class="text-center text-white font-weight-bold card-text"><?php echo $recipe->getname(); ?></p>
+                                </div>
+                            <img class="size_img" src="<?= $path ?>/assets/img/<?php echo $recipe->getPicture(); ?>" alt="Card image cap">
+                        </div>
+                    </a>
+            <?php endforeach; ?>
+
+        </div>
+    </div>     
 </section>
