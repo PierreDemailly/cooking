912b95d29e29ed93bfb970e3465b9fede9f8e016
Paul <decouxpaul0@gmail.Com>
2018-12-18 11:39:59 +0100

    $errors in html

--- views/registerRecipeVue.php
+++ views/registerRecipeVue.php
@@ -6,11 +6,16 @@
                 
                 <p class="form-title">Enregistrer une recette</p>
                         
+                <?php if(isset($errors['recipe-title'])): ?>
+                    <p class="alert alert-warning"><?= $errors['recipe-title'] ?></p>
+                <?php endif; ?>
                 <div class="col-md-12 mx-auto form-group">
                     <input class="form-control" type="text" id="recipe-title" name="recipe-title" placeholder="Exemple: Omelette aux champignons">
                     <label for="recipe-title"><small>Choisissez le titre de votre recette.</small></label>
                 </div>
-
+                <?php if(isset($errors['recipe-pic'])){ ?>
+                    <p><?= $errors['recipe-pic'] ?></p>
+                <?php } ?>
                 <div class="col-md-12 mx-auto form-group">
                 <div class="input-group">
                     <div class="custom-file">
@@ -22,6 +27,10 @@
                 </div>
                 <hr>
             <div class="row mx-auto">
+                <?php if (isset($errors['recipe-type'])) { ?>
+                    <p><?= $errors['recipe-type'] ?></p>
+                <?php 
+            } ?>
                 <div class="col-md-4 mx-auto form-group">
                     <select class="form-control" name="recipe-type" id="recipe-type">
                         <option selected disabled>Type de plat</option>
@@ -37,7 +46,10 @@
                     </select>
                     <label for="recipe-type"><small>Choisissez le type de plat correspondant à votre recette.</small></label>
                 </div>
-
+                <?php if (isset($errors['recipe-difficulty'])) { ?>
+                    <p><?= $errors['recipe-difficulty'] ?></p>
+                <?php 
+            } ?>
                 <div class="col-md-4 mx-auto form-group">
                     <select class="form-control" name="recipe-difficulty" id="recipe-difficulty">
                         <option selected disabled>Difficulté</option>
@@ -48,7 +60,10 @@
                     </select>
                     <label for="recipe-difficulty"><small>Choisissez la difficulté de la recette.</small></label>
                 </div>
-
+                <?php if (isset($errors['recipe-cost'])) { ?>
+                    <p><?= $errors['recipe-cost'] ?></p>
+                <?php 
+            } ?>
                 <div class="col-md-4 mx-auto form-group">
                     <select class="form-control" name="recipe-cost" id="recipe-cost">
                         <option selected disabled>Coût</option>
@@ -62,16 +77,29 @@
                 </div>
 <hr>
                 <div class="row form-group">
+                    <?php if (isset($errors['recipe-preparation-time'])) { ?>
+                    <p><?= $errors['recipe-preparation-time'] ?></p>
+                <?php 
+            } ?>
                     <div class="d-flex inline">
                         <input class="form-control input-number" type="number" name="recipe-preparation-time">
                         <small>Temps de préparation en minutes</small>
                     </div>
+                    <?php if (isset($errors['recipe-cooking-time'])) { ?>
+                    <p><?= $errors['recipe-cooking-time'] ?></p>
+                <?php 
+            } ?>
                     <div class="d-flex inline">
                         <input class="form-control input-number" type="number" name="recipe-cooking-time">
                         <small>Temps de cuisson en minutes</small>
                     </div>
                 </div>
 <hr>
+                <?php if (isset($errors['recipe-for'])) { ?>
+                    <p><?= $errors['recipe-for'] ?></p>
+                <?php 
+            } ?>
+        
                 <div class="row mx-auto inline col-md-12 form-group">
                 
                         <p class="mt-3 mx-3">Pour</p>
@@ -82,18 +110,25 @@
                 </div>
 <hr>
                 <p class="text-center font-weight-bold">Ingrédients (quantité et intitulé)</p>
-                <div id="input_ingredients">
+                <div id="input_ingredientts">
+                    <?php if (isset($errors['recipe-ingredient'])) { ?>
+                    <p><?= $errors['recipe-ingredient'] ?></p>
+                <?php 
+            } ?>
                    <div id="parent-1" class="form-group row mx-auto  col-md-12">
-                            <input class="col-md-3 form-control" type="text" name="recipe-ingredien-quantity" placeholder="Quantité">
+                            <input class="col-md-3 form-control" type="text" name="recipe-ingredient-quantity" placeholder="Quantité">
                             <span class="mt-2 mx-2">de</span>
-                            <input class="col-md-6 form-control" type="text" name="recipe-ingredien" placeholder="Ingrédient">
+                            <input class="col-md-6 form-control" type="text" name="recipe-ingredient" placeholder="Ingrédient">
                             <button type="button" onclick="get_ingredient(this)" class="mx-2 btn btn-info">+</button>
                     </div>
                 </div>
                     <small>Exemple: 5 cuillères à soupe de miel, 200 g de farine, 10 cl de lait... </small>
 <hr>
                 <p class="text-center font-weight-bold">Préparation de la recette</p>
-
+<?php if (isset($errors['recipe-step'])) { ?>
+                    <p><?= $errors['recipe-step'] ?></p>
+                <?php 
+            } ?>
                 <div  id="stepDiv" class="justify-content-center form-group">
 
                     <div class="row">
@@ -104,6 +139,10 @@
                      
                 </div>
 <hr>
+<?php if (isset($errors['recipe-drink'])) { ?>
+                    <p><?= $errors['recipe-drink'] ?></p>
+                <?php 
+            } ?>
                 <div class="row justify-content-center form-group">
                     <input class="col-md-12 form-control" type="text" name="recipe-drink" placeholder="Exemple: vin rouge">
                     <small>Boisson conseillée</small>
