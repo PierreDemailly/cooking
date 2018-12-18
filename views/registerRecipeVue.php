e28a488cbdc5c02358070336f790b2447ca73cfd
Paul <decouxpaul0@gmail.Com>
2018-12-18 11:06:50 +0100

    java script recipeRegisterVue

--- views/registerRecipeVue.php
+++ views/registerRecipeVue.php
@@ -2,43 +2,43 @@
     
     <div class="container p-5">
 
-            <form class="px-5" method="post" enctype="multipart/form-data">
+            <form class="col-12 col-md-8 mx-auto p-5" method="post" enctype="multipart/form-data">
                 
                 <p class="form-title">Enregistrer une recette</p>
                         
-                <div class="form-group">
+                <div class="col-md-12 mx-auto form-group">
                     <input class="form-control" type="text" id="recipe-title" name="recipe-title" placeholder="Exemple: Omelette aux champignons">
                     <label for="recipe-title"><small>Choisissez le titre de votre recette.</small></label>
                 </div>
 
-                <div class="form-group">
+                <div class="col-md-12 mx-auto form-group">
                 <div class="input-group">
                     <div class="custom-file">
                         <input type="file" class="custom-file-input" id="recipe-pic" name="recipe-pic">
                         <label class="custom-file-label" for="recipe-pic">tartecitron.jpg</label>
                     </div>
                 </div>
-                    <small>Ajoutez une photo.<br>Format du fichier: JPG<br>Taille maximale du fichier: 1MO</small>
+                    <small>Ajoutez une photo. / Format du fichier: JPG. / Taille maximale du fichier: 1MO</small>
                 </div>
-                
-
-                <div class="form-group">
+                <hr>
+            <div class="row mx-auto">
+                <div class="col-md-4 mx-auto form-group">
                     <select class="form-control" name="recipe-type" id="recipe-type">
                         <option selected disabled>Type de plat</option>
                         <option value="starter">Entrée</option>
                         <option value="main-course">Plat</option>
                         <option value="dessert">Dessert</option>
                         <option value="side">Accompagnement</option>
-                        <option value="appetize">Amuse gueule</option>
+                        <option value="appetizer">Amuse gueule</option>
                         <option value="drink">Boisson</option>
                         <option value="candy">Confiserie</option>
-                        <option value="sauces">Sauce</option>
+                        <option value="sauce">Sauce</option>
                         <option value="tip">Conseil</option>
                     </select>
                     <label for="recipe-type"><small>Choisissez le type de plat correspondant à votre recette.</small></label>
                 </div>
 
-                <div class="form-group">
+                <div class="col-md-4 mx-auto form-group">
                     <select class="form-control" name="recipe-difficulty" id="recipe-difficulty">
                         <option selected disabled>Difficulté</option>
                         <option value="very-easy">Très facile</option>
@@ -49,7 +49,7 @@
                     <label for="recipe-difficulty"><small>Choisissez la difficulté de la recette.</small></label>
                 </div>
 
-                <div class="form-group">
+                <div class="col-md-4 mx-auto form-group">
                     <select class="form-control" name="recipe-cost" id="recipe-cost">
                         <option selected disabled>Coût</option>
                         <option value="cheap">Bon marché</option>
@@ -59,43 +59,57 @@
                     </select>
                     <label for="recipe-cost"><small>Évaluez le coût de cette recette.</small></label>
                 </div>
-
-                <div class="form-group">
-                    <input class="form-control input-number" type="number" name="recipe-preparation-time">
-                    <small>Temps de préparation en minutes</small>
-                    <input class="form-control input-number" type="number" name="recipe-cooking-time">
-                    <small>Temps de cuisson en minutes</small>
                 </div>
-
-                <div class="form-group">
-                    Pour <input class="form-control input-number" type="number" name="recipe-for-quantity" placeholder="x"> <input class="form-control" type="text" name="recipe-for" placeholder="personne(s)">
+<hr>
+                <div class="row form-group">
+                    <div class="d-flex inline">
+                        <input class="form-control input-number" type="number" name="recipe-preparation-time">
+                        <small>Temps de préparation en minutes</small>
+                    </div>
+                    <div class="d-flex inline">
+                        <input class="form-control input-number" type="number" name="recipe-cooking-time">
+                        <small>Temps de cuisson en minutes</small>
+                    </div>
+                </div>
+<hr>
+                <div class="row mx-auto inline col-md-12 form-group">
+                
+                        <p class="mt-3 mx-3">Pour</p>
+                        <input class="mr-2 col-md-2 form-control input-number" type="number" name="recipe-for-quantity" placeholder="x"> 
+                        <input class="col-md-8 form-control" type="text" name="recipe-for" placeholder="personne(s)">
+                    
                     <small>Exemple: Pour 5 personnes, pour 1 litre, pour 6 pièces...</small>
                 </div>
-
-                <p>Ingrédients (quantité et intitulé)</p>
-
-                <div class="form-group">
-                    <input class="form-control" type="text" name="recipe-ingredien-quantity" placeholder="Quantité">
-                    de
-                    <input class="form-control" type="text" name="recipe-ingredien" placeholder="Ingrédient">
+<hr>
+                <p class="text-center font-weight-bold">Ingrédients (quantité et intitulé)</p>
+                <div id="input_ingredients">
+                   <div id="parent-1" class="form-group row mx-auto  col-md-12">
+                            <input class="col-md-3 form-control" type="text" name="recipe-ingredien-quantity" placeholder="Quantité">
+                            <span class="mt-2 mx-2">de</span>
+                            <input class="col-md-6 form-control" type="text" name="recipe-ingredien" placeholder="Ingrédient">
+                            <button type="button" onclick="get_ingredient(this)" class="mx-2 btn btn-info">+</button>
+                    </div>
+                </div>
                     <small>Exemple: 5 cuillères à soupe de miel, 200 g de farine, 10 cl de lait... </small>
-                    <button type="button" class="btn btn-info">+</button>
+<hr>
+                <p class="text-center font-weight-bold">Préparation de la recette</p>
+
+                <div  id="stepDiv" class="justify-content-center form-group">
+
+                    <div class="row">
+                        <strong class="mt-2 mx-2">Étape 1:</strong>
+                        <textarea class="col-md-9 form-control" type="text" rows="1" name="recipe-step" placeholder="Exemple: Mélanger le lait avec la farine"></textarea>
+                        <button type="button" onclick="get_step(this)" class="mx-2 col-md-1 btn btn-info">+</button>
+                    </div>
+                     
                 </div>
-
-                <p>Préparation de la recette</p>
-
-                <div class="form-group">
-                    <strong>Étape 1:</strong>
-                    <input class="form-control" type="text" name="recipe-step" placeholder="Exemple: Mélanger le lait avec la farine">
-                    <button type="button" class="btn btn-info">+</button>
-                </div>
-
-                <div class="form-group">
-                    <input class="form-control" type="text" name="recipe-drink" placeholder="Exemple: vin rouge">
+<hr>
+                <div class="row justify-content-center form-group">
+                    <input class="col-md-12 form-control" type="text" name="recipe-drink" placeholder="Exemple: vin rouge">
                     <small>Boisson conseillée</small>
                 </div>
 
-                <button class="btn btn-success" name="recipe-submit">Envoyer</button>
+                <button class="btn btn-success col-md-12" name="recipe-submit">Envoyer</button>
         </form>
 
     </div>
